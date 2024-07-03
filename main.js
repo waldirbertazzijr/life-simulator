9362479e142396211adcaab39cace1a53b9b5af6
Waldir Bertazzi Junior <waldirbj@gmail.com>
2024-07-02 22:56:56 -0300

    Some features.
    - Adding tooltip to inspect the animal.
    - Adding family badge.
    - Refactoring configs.

--- main.js
+++ main.js
@@ -1,31 +1,78 @@
 // Simulation configuration
-const config = {
-    drawTargets: true,
-    
-    energyBar: {
-        enabled: true,
-        width: 30,
-        height: 2,
-    },
-
-    procreationAnimations: {
-        enabled: true,
-        radius: 10
-    },
-
-    worldWidth: 1200,
-    worldHeight: 600,
-    treePopulation: [200, 300],
-    herbivorePopulation: [20, 30],
-    carnivorePopulation: [5, 10],
+let config = {
     run: true,
+    canvas: null,
+
+    visuals: {
+        procreationAnimations: {
+            enabled: true,
+            radius: 10
+        },
+        drawTargets: true,
+        energyBar: {
+            enabled: true,
+            width: 30,
+            height: 2,
+        },
+    },
+
+    world: {
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+
+    trees: {
+        initialPopulation: [200, 300],
+        energyGain: 0.001,
+        procreatingChance: 0,
+        energy: {
+            initial: 1000,
+            max: 1000,
+        }
+    },
+    herbivores: {
+        initialPopulation: [20, 100],
+        energyGain: -0.25,
+        procreatingChance: 0.00001,
+        clockAction: 2,
+        energy: {
+            initial: [100, 500],
+            max: 500,
+        }
+    },
+    carnivores: {
+        initialPopulation: [5, 10],
+        energyGain: -0.1,
+        procreatingChance: 0.001,
+        clockAction: 0.1,
+        energy: {
+            initial: [50, 100],
+            max: 100,
+        }
+    },
+
+    energy: {
+        defaultEnergy: 100,
+        defaultMaxEnergy: 500,
+    },
+
+    images: {
+        tree: './tree.png',
+        herbivore: './herbivore.png',
+        carnivore: './carnivore.png',
+        background: './grass.png'
+    }
 };
 
 // Utility functions
-const randomInt = (min, max) => min + Math.floor((max - min) * Math.random());
+const randomInt = (min, max) => min + Math.floor((max - min) * Math.random()) + 1;
 
 const removeDead = () => {
     animals = animals.filter(animal => animal.energy > 0);
+};
+
+const randomHexColor = () => {
+    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
 };
 
 // Animal array
@@ -33,29 +80,29 @@ let animals = [];
 
 // Load images
 const treeImage = new Image();
-treeImage.src = './tree.png';
+treeImage.src = config.images.tree;
 
 const herbivoreImage = new Image();
-herbivoreImage.src = './herbivore.png';
+herbivoreImage.src = config.images.herbivore;
 
 const carnivoreImage = new Image();
-carnivoreImage.src = './carnivore.png';
+carnivoreImage.src = config.images.carnivore;
 
 class Animal {
-    constructor(kind, energyGain, procriationChance, walkStrategy = 'static', image) {
+    constructor(kind, energyGain, procriationChance, walkStrategy = 'static', image, initialEnergy, maxEnergy, clockAction) {
         this.kind = kind;
-        this.positionX = randomInt(0, config.worldWidth);
-        this.positionY = randomInt(0, config.worldHeight);
-        this.clockAction = 1;
+        this.positionX = randomInt(0, config.world.width);
+        this.positionY = randomInt(0, config.world.height);
+        this.clockAction = clockAction;
         this.clock = 0;
-        this.energy = 100;
-        this.maxEnergy = 500;
+        this.energy = Array.isArray(initialEnergy) ? randomInt(...initialEnergy) : initialEnergy;
+        this.maxEnergy = maxEnergy;
         this.energyGain = energyGain;
         this.procriationChance = procriationChance;
         this.walkStrategy = walkStrategy;
         this.currentTarget = null;
         this.consumes = [];
-        this.family = `_${Math.random().toString(36).substring(2, 9)}`;
+        this.family = randomHexColor();
         this.image = image;
     }
 
@@ -104,7 +151,7 @@ class Animal {
 
     flee() {
         this.currentTarget = null;
-        
+
         let closestHunter = null;
         let closestHunterDistance = Infinity;
 
@@ -138,16 +185,16 @@ class Animal {
     procriate() {
         if (Math.random() <= this.procriationChance) {
             this.energy -= this.energy / 3;
-    
+
             const child = AnimalFactory.getAnimal(this.kind);
             child.positionX = this.positionX + randomInt(-10, 10);
             child.positionY = this.positionY + randomInt(-10, 10);
             child.family = this.family;
-    
+
             animals.push(child);
-    
+
             // Trigger the procreation animation
-            if(config.procreationAnimations.enabled) createProcreationAnimation(child.positionX, child.positionY);
+            if (config.visuals.procreationAnimations.enabled) createProcreationAnimation(child.positionX, child.positionY);
         }
     }
 
@@ -171,7 +218,7 @@ class Animal {
             }
         }
 
-        if (closestHunterDistance < 50) {
+        if (closestHunterDistance < 100) {
             this.walkStrategy = 'fleeing';
         } else if (this.energy / this.maxEnergy <= 0.5) {
             this.walkStrategy = 'seekFood';
@@ -196,55 +243,55 @@ class Animal {
     }
 
     draw(ctx) {
+        // Draw the animal image
         ctx.drawImage(this.image, this.positionX - 16, this.positionY - 16, 32, 32);
-        ctx.font = '8px Monaco';
-        ctx.fillStyle = '#000';
-        
-        if (this.walkStrategy !== 'static') ctx.fillText(`${this.walkStrategy}`, this.positionX - 16, this.positionY + 16);
 
-        if (config.energyBar.enabled && this.walkStrategy !== 'static') {
-            const energyPercentage = this.energy / this.maxEnergy;
-            const barColor = energyPercentage > 0.2 ? 'green' : 'red';
-            ctx.fillStyle = barColor;
-            ctx.fillRect(this.positionX - config.energyBar.width / 2, this.positionY - 20, config.energyBar.width * energyPercentage, config.energyBar.height);
-            ctx.strokeStyle = '#000';
-            ctx.strokeRect(this.positionX - config.energyBar.width / 2, this.positionY - 20, config.energyBar.width, config.energyBar.height);
-        }
-
-        if (config.drawTargets && this.currentTarget) {
+        if (this.walkStrategy !== 'static') {
+            // Draw the family circle
+            ctx.fillStyle = this.family;
             ctx.beginPath();
-            ctx.moveTo(this.positionX, this.positionY);
-            ctx.lineTo(this.currentTarget.positionX, this.currentTarget.positionY);
+            ctx.arc(this.positionX - 20, this.positionY - 19, 2, 0, 2 * Math.PI);
+            ctx.strokeStyle = "#000";
             ctx.stroke();
+            ctx.fill();
+        
+
+            if (config.visuals.energyBar.enabled) {
+                const energyPercentage = this.energy / this.maxEnergy;
+                const barColor = energyPercentage > 0.2 ? 'green' : 'red';
+                ctx.fillStyle = barColor;
+                ctx.fillRect(this.positionX - config.visuals.energyBar.width / 2, this.positionY - 20, config.visuals.energyBar.width * energyPercentage, config.visuals.energyBar.height);
+                ctx.strokeStyle = '#000';
+                ctx.strokeRect(this.positionX - config.visuals.energyBar.width / 2, this.positionY - 20, config.visuals.energyBar.width, config.visuals.energyBar.height);
+            }
+
+            if (config.visuals.drawTargets && this.currentTarget) {
+                ctx.beginPath();
+                ctx.moveTo(this.positionX, this.positionY);
+                ctx.lineTo(this.currentTarget.positionX, this.currentTarget.positionY);
+                ctx.stroke();
+            }
         }
     }
 }
 
 class Tree extends Animal {
     constructor() {
-        super('tree', 0.01, 0.00005, 'static', treeImage);
-        this.energy = 1000;
-        this.maxEnergy = 1000;
+        super('tree', config.trees.energyGain, config.trees.procreatingChance, 'static', treeImage, config.trees.energy.initial, config.trees.energy.max);
     }
 }
 
 class Herbivore extends Animal {
     constructor() {
-        super('herbivore', -0.25, 0.00001, 'seekFood', herbivoreImage);
+        super('herbivore', config.herbivores.energyGain, config.herbivores.procreatingChance, 'seekFood', herbivoreImage, config.herbivores.energy.initial, config.herbivores.energy.max, config.herbivores.clockAction);
         this.consumes = ['tree'];
-        this.clockAction = 2;
-        this.energy = randomInt(100, 500);
-        this.maxEnergy = 500;
     }
 }
 
 class Carnivore extends Animal {
     constructor() {
-        super('carnivore', -0.1, 0.001, 'seekFood', carnivoreImage);
+        super('carnivore', config.carnivores.energyGain, config.carnivores.procreatingChance, 'seekFood', carnivoreImage, config.carnivores.energy.initial, config.carnivores.energy.max, config.carnivores.clockAction);
         this.consumes = ['herbivore', 'carnivore'];
-        this.clockAction = 0.1;
-        this.energy = randomInt(50, 100);
-        this.maxEnergy = 100;
     }
 }
 
@@ -270,15 +317,15 @@ const populate = (kind, range) => {
     }
 };
 
-populate('tree', config.treePopulation);
-populate('herbivore', config.herbivorePopulation);
-populate('carnivore', config.carnivorePopulation);
+populate('tree', config.trees.initialPopulation);
+populate('herbivore', config.herbivores.initialPopulation);
+populate('carnivore', config.carnivores.initialPopulation);
 
 const stats = new Stats();
 stats.showPanel(0);
 
 const bg = new Image();
-bg.src = './grass.png';
+bg.src = config.images.background;
 
 const drawBackground = () => {
     window.ctx.drawImage(bg, 0, 0);
@@ -296,7 +343,7 @@ const createProcreationAnimation = (x, y) => {
 
 const draw = () => {
     stats.begin();
-    window.ctx.clearRect(0, 0, config.worldWidth, config.worldHeight);
+    window.ctx.clearRect(0, 0, config.world.width, config.world.height);
 
     animals.forEach(animal => {
         animal.live();
@@ -325,13 +372,90 @@ const draw = () => {
     window.requestAnimationFrame(draw);
 };
 
+const fixCanvasSize = () => {
+    config.world.width = window.innerWidth;
+    config.world.height = window.innerHeight;
+
+    config.canvas.width = config.world.width;
+    config.canvas.height = config.world.height;
+}
+
 const init = () => {
-    window.ctx = document.getElementById('canvas').getContext('2d');
+    config.canvas = document.getElementById('canvas');
+
+    fixCanvasSize();
+
+    window.ctx = config.canvas.getContext('2d');
     window.ctx.globalCompositeOperation = 'destination-over';
     window.requestAnimationFrame(draw);
+
+    new Tooltip();
 };
 
 window.onload = () => {
     document.body.appendChild(stats.dom);
     init();
 };
+
+class Tooltip {
+    constructor() {
+        this.tooltip = document.createElement('div');
+        this.tooltip.id = 'tooltip';
+        this.tooltip.style.position = 'absolute';
+        this.tooltip.style.display = 'none';
+        this.tooltip.style.font = '6px Helvetica, Arial';
+        this.tooltip.style.backgroundColor = 'white';
+        this.tooltip.style.border = '1px solid black';
+        this.tooltip.style.borderRadius = '5px';
+        this.tooltip.style.boxShadow = '0 5px 10px rgba(0,0,0,0.5)';
+        this.tooltip.style.padding = '5px';
+        this.tooltip.style.fontSize = '12px';
+        this.tooltip.style.pointerEvents = 'none';
+        this.tooltip.style.zIndex = '1';
+        document.body.appendChild(this.tooltip);
+
+        this.canvas = document.getElementById('canvas');
+        this.canvasRect = this.canvas.getBoundingClientRect();
+
+        this.canvas.addEventListener('mousemove', (event) => this.onMouseMove(event));
+        this.canvas.addEventListener('mouseout', () => this.onMouseOut());
+    }
+
+    onMouseMove(event) {
+        const mouseX = event.clientX - this.canvasRect.left;
+        const mouseY = event.clientY - this.canvasRect.top;
+
+        let hoveredAnimal = null;
+        for (const animal of animals) {
+            const distance = Math.hypot(animal.positionX - mouseX, animal.positionY - mouseY);
+            if (distance < 16) {
+                hoveredAnimal = animal;
+                break;
+            }
+        }
+
+        if (hoveredAnimal) {
+            this.tooltip.style.display = 'block';
+            this.tooltip.style.left = `${event.pageX + 10}px`;
+            this.tooltip.style.top = `${event.pageY + 10}px`;
+            
+            const properties = Object.entries(hoveredAnimal)
+                .filter(([key, value]) => (key !== 'currentTarget' && key !== 'image' && key !== 'clock' && key !== 'kind'))
+                .map(([key, value]) => {
+                    if (typeof value === 'number') {
+                        return `${key}: ${value.toFixed(2)}`;
+                    }
+                    return `${key}: ${JSON.stringify(value)}`;
+                })
+                .join('</br>');
+
+            this.tooltip.innerHTML = `<strong>${hoveredAnimal.kind}</strong><br>${properties}`;
+        } else {
+            this.tooltip.style.display = 'none';
+        }
+    }
+
+    onMouseOut() {
+        this.tooltip.style.display = 'none';
+    }
+}
